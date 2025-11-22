74f07f392808bcf5453cb9e04223433818a5cce2
yves george <ifi1960@gmail.com>
2025-11-22 20:29:19 +0100

    Initial Commit

---
+++ Suivi Sportif/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Suivi Sportif
+//
+//  Created by yves george on 22/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Suivi Sportif/Suivi_SportifApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suivi_SportifApp.swift
+//  Suivi Sportif
+//
+//  Created by yves george on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Suivi_SportifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
